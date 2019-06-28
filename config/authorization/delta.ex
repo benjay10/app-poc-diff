d6bbf87dfa2be3298abecaa7d20132336a47fee3
Aad Versteden <madnificent@gmail.com>
2019-06-28 17:11:23 +0200

    Add producer and delta support
    This is a WIP, ready to be tested.

---
+++ config/authorization/delta.ex
@@ -0,0 +1,20 @@
+defmodule Delta.Config do
+  @moduledoc """
+  Configuration for the Delta service.  This file describes which
+  components should be informed.
+  """
+
+  @type target :: String.t
+
+  @doc """
+  Links to each endpoint to which the delta's will be sent.
+  Optionally containing ports.
+  """
+  @spec targets() :: [target]
+  def targets do
+    [
+      "http://deltanotifier"
+     ]
+  end
+
+end
---
+++ config/delta-producer/rules.js
@@ -0,0 +1,28 @@
+export default [
+  {
+    match: {
+      // form of element is {subject,predicate,object}
+    },
+    callback: {
+      url: "http://producer/delta", method: "POST"
+    },
+    options: {
+      resourceFormat: "v0.0.1",
+      gracePeriod: 1000
+    }
+  },
+  {
+    match: {
+      // form of element is {subject,predicate,object}
+      // predicate: { type: "uri", value: "http://www.semanticdesktop.org/ontologies/2007/03/22/nmo#isPartOf" }
+    },
+    callback: {
+      url: "http://resource/.mu/delta", method: "POST"
+    },
+    options: {
+      resourceFormat: "v0.0.1",
+      gracePeriod: 5000,
+      ignoreFromSelf: true
+    }
+  }
+];
