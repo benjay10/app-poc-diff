aec50b2ebc50d1af71da64aea2fc5ce401dbe906
Aad Versteden <madnificent@gmail.com>
2020-06-10 23:10:00 +0200

    Upgrade to dispatcher v2.0.0 and new identifier
    Upgrading the identifier and the dispatcher to new versions.

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -1,44 +1,53 @@
 defmodule Dispatcher do
-  use Plug.Router
+  use Matcher
 
-  def start(_argv) do
-    port = 80
-    IO.puts "Starting Plug with Cowboy on port #{port}"
-    Plug.Adapters.Cowboy.http __MODULE__, [], port: port
-    :timer.sleep(:infinity)
+  define_accept_types [
+    json: [ "application/json", "application/vnd.api+json" ],
+    html: [ "text/html", "application/xhtml+html" ],
+    any: [ "*/*" ]
+  ]
+
+  @json %{ accept: %{ json: true } }
+  @any %{ accept: %{ any: true } }
+  @html %{ accept: %{ html: true } }
+
+  match "/books/*path", @json do
+    forward conn, path, "http://resource/books/"
+  end
+  match "/authors/*path", @json do
+    forward conn, path, "http://resource/authors/"
   end
 
-  plug Plug.Logger
-  plug :match
-  plug :dispatch
-
-  # In order to forward the 'themes' resource to the
-  # resource service, use the following forward rule.
-  #
-  # docker-compose stop; docker-compose rm; docker-compose up
-  # after altering this file.
-  #
-  # match "/themes/*path" do
-  #   Proxy.forward conn, path, "http://resource/themes/"
-  # end
-
-  match "/books/*path" do
-    Proxy.forward conn, path, "http://resource/books/"
-  end
-  match "/authors/*path" do
-    Proxy.forward conn, path, "http://resource/authors/"
+  get "/files/:id/download", @any do
+    forward conn, [], "http://file/files/" <> id <> "/download"
   end
 
-  get "/files/:id/download" do
-    Proxy.forward conn, [], "http://file/files/" <> id <> "/download"
+  get "/sync/files/*path", @any do
+    forward conn, path, "http://producer/files/"
   end
 
-  get "/sync/files/*path" do
-    Proxy.forward conn, path, "http://producer/files/"
+  # host the frontend
+
+  match "/assets/*path", @any do
+    IO.puts "hi"
+    forward conn, path, "http://frontend/assets/"
   end
 
-  match _ do
+  match "/favicon.ico", _ do
+    IO.puts "hi"
+    forward conn, [], "http://frontend/assets/favicon.ico"
+  end
+
+  match "/*path", @html do
+    IO.puts "hi"
+    IO.inspect( path, label: "Path to match:" )
+    forward conn, [], "http://frontend/index.html"
+  end
+  
+  # 404 - File not found
+
+  match "/*_path", %{ last_call: true } do
+    IO.puts "hi"
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
-
 end
